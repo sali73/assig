3a07477aebb0235865769006a32b03f625201f19
sali73 <sally.elgendy@hotmail.com>
2020-10-17 20:42:00 -0400

    fix componont name in app.js also delete css file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import  WorkerList from './Components/Workers'
+
+
+export class App extends Component {
+  render() {
+    return (
+      
+        <WorkerList/>
+
+    );
+  }
+}
+
+
